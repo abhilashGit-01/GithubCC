d62fec20b6b4fb0060b52d71cf558f917cdf5ef3
abhilashGit-01 <srabhilashgit@gmail.com>
2025-10-19 16:09:41 +0530

    first commit

---
+++ second.java
@@ -0,0 +1,14 @@
+class point{
+    int x,y;
+}
+public class second {
+    public static void main(String[] args) {
+        point p=new point();
+        p.x=10;
+        p.y=20;
+        System.out.println(p.x);
+        System.out.println(p.y);
+        //some comments
+        
+    }
+}
