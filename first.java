72ec27e201e55f610cb55f08fc1357337538cb8f
abhilashGit-01 <srabhilashgit@gmail.com>
2025-10-19 17:08:28 +0530

    second commit

---
+++ first.java
@@ -0,0 +1,8 @@
+public class first{
+    public static void main(String[] args){
+        System.out.println("hello fucking world");
+        int abhi=22;
+        System.out.println(abhi);
+
+    }
+}
